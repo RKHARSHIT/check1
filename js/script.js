f1ca6b09760e23fd0eb67c80f42373c3d0334ed2
NischayAhlawat <589harshitarora.2020@gmail.com>
2023-01-11 09:21:57 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ $(function () {// Same as document.addEventListner("DOMContentLoaded"...)
         }
     }); 
 });
+
+document.addEventListener("click", function(e){
+    if(e.target.classList.contains("pd-img")){
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
+        myModal.show()
+    }
+})
