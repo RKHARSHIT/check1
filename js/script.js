6eed39d12c00d73ada44e977dbb8f13329dab3ec
NischayAhlawat <589harshitarora.2020@gmail.com>
2023-01-15 05:38:01 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 $(function () {// Same as document.addEventListner("DOMContentLoaded"...)
 
     // Same as document.querySelector("#navbarToggle").addEventListener("blur")
-    $(".navbar-toggler").blur(function (event){
+    $(".navbar-toggler").blur(function (event) {
         var screenWidth = window.innerWidth;
-        if(screenWidth < 992){
+        if (screenWidth < 992) {
             $('#navbarNav').collapse('hide');
         }
-    }); 
+    });
 });
 
-document.addEventListener("click", function(e){
-    if(e.target.classList.contains("pd-img")){
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("pd-img")) {
         const src = e.target.getAttribute("src");
         document.querySelector(".modal-img").src = src;
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show()
     }
-})
+});
+
+function revealx() {
+    var reveals = document.querySelectorAll(".revealx");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealx);
+
+function revealy() {
+    var reveals = document.querySelectorAll(".revealy");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealy);
